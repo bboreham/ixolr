ea29ce1000ef63794ee7abff48409b6c6c08723c
Bryan Boreham <bjboreham@gmail.com>
2024-04-28 14:39:16 +0100

    AppDelegate: remove some boilerplate comments

--- iXolr/iXolrAppDelegate.m
+++ iXolr/iXolrAppDelegate.m
@@ -377,10 +377,6 @@ NSString * const IXSettingUseDynamicType = @"useDynamicType";
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-    /*
-     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-     */
     NSLog(@"applicationWillResignActive");
     [self removeRefreshTimer];
 	[_CIXhostReach stopNotifier];
@@ -388,10 +384,6 @@ NSString * const IXSettingUseDynamicType = @"useDynamicType";
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    /*
-     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-     If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-     */
     NSLog(@"applicationDidEnterBackground");
     _notification = nil;
     if (self.settings.outboxAlert && [self.dataController outboxMessageCountToUpload] > 0) {
@@ -416,18 +408,12 @@ NSString * const IXSettingUseDynamicType = @"useDynamicType";
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
-    /*
-     Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-     */
     NSLog(@"applicationWillEnterForeground");
     [self cancelOutboxNotification];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-    /*
-     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-     */
     NSLog(@"applicationDidBecomeActive");
     if (_authStr == nil || consumer == nil)
         [self oauthInitAuthorization];
@@ -437,7 +423,6 @@ NSString * const IXSettingUseDynamicType = @"useDynamicType";
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-    // Saves changes in the application's managed object context before the application terminates.
     [self saveState];
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
